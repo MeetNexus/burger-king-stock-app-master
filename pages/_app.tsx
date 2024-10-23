395f8c7ce80173bc6580c60a435024fd50edeb9b
MeetNexus <meetmap.services@gmail.com>
2024-10-23 03:30:32 +0200

    0.0.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
+    <html lang="en">
+      <head>
+        <title>Next.js App</title>
+      </head>
+      <body>
+        <Navbar />
+        <Component {...pageProps} />
+        <SpeedInsights />
+      </body>
+    </html>
   )
 }
